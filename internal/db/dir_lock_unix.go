95541a81c7af326def710f9fde30e59267a95503
RUDRA-PRATAP-SINGH01 <rudrapratap9256@gmail.com>
2026-06-23 22:27:27 +0530

    fix(lock): map Unix flock errors to ErrDatabaseLocked

--- internal/db/dir_lock_unix.go
+++ internal/db/dir_lock_unix.go
@@ -8,7 +8,16 @@ import (
 )
 
 func lockFile(f *os.File) error {
-	return syscall.Flock(int(f.Fd()), syscall.LOCK_EX|syscall.LOCK_NB)
+	err := syscall.Flock(int(f.Fd()), syscall.LOCK_EX|syscall.LOCK_NB)
+	if err == nil {
+		return nil
+	}
+	if errno, ok := err.(syscall.Errno); ok {
+		if errno == syscall.EWOULDBLOCK || errno == syscall.EAGAIN {
+			return ErrDatabaseLocked
+		}
+	}
+	return err
 }
 
 func unlockFile(f *os.File) error {
